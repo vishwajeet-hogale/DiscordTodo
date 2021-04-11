a6d6246cab62baffd2e66cbbc2e8c68e51f011f6
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-04-11 15:56:30 +0530

    Learning how to use discord in python

--- my_bot.py
+++ my_bot.py
@@ -10,6 +10,12 @@ async def on_ready():
 async def on_message(message):
     if message.content == 'What is the version':
         general_channel = client.get_channel(830510316659408936)
-        await general_channel.send("The version is 1.0")
+        
+        myEmbed = discord.Embed(title="Current Version ",description= "The bot is in Version 1.0",color=0x00ff00)
+        myEmbed.add_field(name="Version code ",value="v1.0.0",inline=False )
+        myEmbed.add_field(name= "Date Released ",value="11th April, 2021",inline=False)
+        myEmbed.set_footer(text="This is a bot that helps you makes your todo lists! ")
+        myEmbed.set_author(name="Vishwajeet Hogale")
+        await general_channel.send(embed=myEmbed)
  
 client.run("ODMwNTAyMTY4NDU2NzI0NTAw.YHHnag.OnS0C0uEeJomAfLi2y5QVdO1GMs")
