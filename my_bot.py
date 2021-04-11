b64b5ae8c3c08c29c25eb7798f904a0f52777717
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-04-11 20:18:16 +0530

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -1,6 +1,19 @@
 import discord 
+from discord.ext import commands
+# client = discord.Client()
+client = commands.Bot(command_prefix = '//')
 
-client = discord.Client()
+@client.command(name='version')
+async def version(context):
+    
+        
+    myEmbed = discord.Embed(title="Current Version ",description= "The bot is in Version 1.0",color=0x00ff00)
+    myEmbed.add_field(name="Version code ",value="v1.0.0",inline=False )
+    myEmbed.add_field(name= "Date Released ",value="11th April, 2021",inline=False)
+    myEmbed.set_footer(text="This is a bot that helps you makes your todo lists! ")
+    myEmbed.set_author(name="Vishwajeet Hogale")
+    await context.message.channel.send(embed=myEmbed)
+
 @client.event
 async def on_ready():
   
@@ -17,5 +30,6 @@ async def on_message(message):
         myEmbed.set_footer(text="This is a bot that helps you makes your todo lists! ")
         myEmbed.set_author(name="Vishwajeet Hogale")
         await general_channel.send(embed=myEmbed)
+    await client.process_commands(message)
  
 client.run("ODMwNTAyMTY4NDU2NzI0NTAw.YHHnag.OnS0C0uEeJomAfLi2y5QVdO1GMs")
